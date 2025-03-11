b17fab5e9e0c7691af184c4326c0e5702e6daa4d
John-Paul Navarro <navarro@anl.gov>
2025-03-11 07:42:23 -0500

    Major updates for 2025-03-10

--- Operations_Dashboard_Django/IntegrationBadgesUI/views.py
+++ Operations_Dashboard_Django/IntegrationBadgesUI/views.py
@@ -1,12 +1,24 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.shortcuts import render
+from django.conf import settings
+import os
+import json
 
 
 def index(request, rest_of_path=None):
-    return render(request, 'IntegrationBadgesUI/index.html', {})
+    django_app_name = "IntegrationBadgesUI"
+    react_app_name = "integration-badges-ui-react"
 
-    # template = loader.get_template("/IntegrationBadgesUI/index")
-    # context = {}
-    # return HttpResponse(template.render(context, request))
+    with open(os.path.join(settings.BASE_DIR,
+                           f"{django_app_name}/static/{react_app_name}/.vite/manifest.json"), 'r') as f:
 
+        manifest_json = json.load(f)
+
+        main_js = manifest_json["index.html"]["file"]
+        main_css = manifest_json["index.html"]["css"][0]
+
+    return render(request, 'IntegrationBadgesUI/index.html', {
+        "main_js": f"{react_app_name}/{main_js}",
+        "main_css": f"{react_app_name}/{main_css}"
+    })
