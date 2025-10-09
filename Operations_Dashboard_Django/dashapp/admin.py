6c3b28755ed359f628e1be4a2a5454cef6e46210
John-Paul Navarro <navarro@anl.gov>
2025-10-09 13:13:35 -0500

    See CHANGELOG tag v0.19.0

--- Operations_Dashboard_Django/dashapp/admin.py
+++ Operations_Dashboard_Django/dashapp/admin.py
@@ -9,26 +9,31 @@ class DashApp_Admin(admin.ModelAdmin):
     list_display_links = ['path']
     ordering = ['path']
     search_fields = ['path', 'name', 'template']
+    save_as = True
     formfield_overrides = {
         models.TextField: {'widget': Textarea},
     }
 admin.site.register(DashApp, DashApp_Admin)
 
 class AppCode_Admin(admin.ModelAdmin):
-    list_display = ['name', 'code_id']
+    list_display = ['name','alias', 'code_id']
     list_display_links = ['name']
     ordering = ['name']
-    search_fields = ['name']
+    search_fields = ['name', 'alias']
+    save_as = True
     formfield_overrides = {
         models.TextField: {'widget': Textarea(attrs={'rows': 40, 'cols': 100})},
     }
 admin.site.register(AppCode, AppCode_Admin)
 
 class DashAppCode_Admin(admin.ModelAdmin):
-    list_display = ['dashapp', 'appcode', 'id']
+    list_display = ['dashapp', 'appcode', 'get_appcode_alias', 'id']
     list_display_links = ['id']
     ordering = ['dashapp', 'appcode']
     search_fields = ['dashapp', 'appcode']
+    def get_appcode_alias(self, obj):
+        return obj.appcode.alias
+    get_appcode_alias
 admin.site.register(DashAppCode, DashAppCode_Admin)
 
 class DashAppGroup_Admin(admin.ModelAdmin):
