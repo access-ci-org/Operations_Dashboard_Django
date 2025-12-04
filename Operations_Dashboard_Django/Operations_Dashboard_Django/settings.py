1f124130597551a9dd1a25a1070d42c1db32a10a
John-Paul Navarro <navarro@anl.gov>
2025-12-04 17:48:06 -0600

    Remove custom search_path in postgres connection

--- Operations_Dashboard_Django/Operations_Dashboard_Django/settings.py
+++ Operations_Dashboard_Django/Operations_Dashboard_Django/settings.py
@@ -21,15 +21,15 @@ import json
 import os
 import sys
 
-if 'APP_CONFIG' not in os.environ:
-    print('Missing APP_CONFIG environment variable')
+if "APP_CONFIG" not in os.environ:
+    print("Missing APP_CONFIG environment variable")
     sys.exit(1)
 try:
-    with open(os.environ['APP_CONFIG'], 'r') as file:
-        conf=file.read()
+    with open(os.environ["APP_CONFIG"], "r") as file:
+        conf = file.read()
     CONF = json.loads(conf)
 except (ValueError, IOError) as e:
-    print('Failed to load APP_CONFIG={}'.format(os.environ['APP_CONFIG']))
+    print("Failed to load APP_CONFIG={}".format(os.environ["APP_CONFIG"]))
     raise
 #####
 
@@ -37,36 +37,36 @@ except (ValueError, IOError) as e:
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = CONF['DJANGO_SECRET_KEY']
+SECRET_KEY = CONF["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = CONF['DEBUG']
+DEBUG = CONF["DEBUG"]
 
-ALLOWED_HOSTS = CONF['ALLOWED_HOSTS']
+ALLOWED_HOSTS = CONF["ALLOWED_HOSTS"]
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.messages',
-    'django.contrib.sessions',
-    'django.contrib.staticfiles',
-    'django_bootstrap5',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.messages",
+    "django.contrib.sessions",
+    "django.contrib.staticfiles",
+    "django_bootstrap5",
     # For django-allauth
-    'django.contrib.sites',
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.cilogon',
+    "django.contrib.sites",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
+    "allauth.socialaccount.providers.cilogon",
     # Our Apps
-    'dashboard_tools',
-    'access_django_user_admin',
-    'badgetoken',
-    'dashapp',
-    'dbfile',
-    'web',
+    "dashboard_tools",
+    "access_django_user_admin",
+    "badgetoken",
+    "dashapp",
+    "dbfile",
+    "web",
 ]
 
 SITE_ID = 1
@@ -84,130 +84,132 @@ SITE_ID = 1
 #    }
 # }
 
-ACCOUNT_EMAIL_VERIFICATION = 'none'
-ACCOUNT_LOGOUT_REDIRECT_URL = '/accounts/cilogon/login/'
-LOGIN_URL = '/accounts/cilogon/login/'
-LOGIN_REDIRECT_URL = '/'
-#SOCIALACCOUNT_ADAPTER = 'dashboard.views.MySocialAccountAdapter'
+ACCOUNT_EMAIL_VERIFICATION = "none"
+ACCOUNT_LOGOUT_REDIRECT_URL = "/accounts/cilogon/login/"
+LOGIN_URL = "/accounts/cilogon/login/"
+LOGIN_REDIRECT_URL = "/"
+# SOCIALACCOUNT_ADAPTER = 'dashboard.views.MySocialAccountAdapter'
 SOCIALACCOUNT_STORE_TOKENS = True
 SOCIALACCOUNT_QUERY_EMAIL = True
 
-#SESSION COOKIE needs to be readable by the IntegrationBadgesUI webapp
+# SESSION COOKIE needs to be readable by the IntegrationBadgesUI webapp
 SESSION_COOKIE_HTTPONLY = False
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
     # 'allauth.account.middleware.AccountMiddleware',
 ]
 
 AUTHENTICATION_BACKENDS = [
     # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
-
+    "django.contrib.auth.backends.ModelBackend",
     # `allauth` specific authentication methods, such as login by e-mail
-    'allauth.account.auth_backends.AuthenticationBackend',
+    "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
-CSRF_TRUSTED_ORIGINS = ["https://localhost", "https://localhost:8443", "https://*.access-ci.org"]
+CSRF_TRUSTED_ORIGINS = [
+    "https://localhost",
+    "https://localhost:8443",
+    "https://*.access-ci.org",
+]
 USE_X_FORWARDED_HOST = True
 USE_X_FORWARDED_PORT = True
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-#SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+# SECURE_SSL_REDIRECT = True
 
 # Switched to True on 09-03-2020 by JP, making whitelist no longer relevant
-#CORS_ORIGIN_ALLOW_ALL = True
-#CORS_ORIGIN_WHITELIST = (
+# CORS_ORIGIN_ALLOW_ALL = True
+# CORS_ORIGIN_WHITELIST = (
 #    'https://access-ci.org',
 #    'https://allocations.access-ci.org',
 #    'https://support.access-ci.org',
 #    'https://operations.access-ci.org',
 #    'https://metrics.access-ci.org',
-#)
-#CORS_ALLOW_METHODS = (
+# )
+# CORS_ALLOW_METHODS = (
 #    'GET'
-#)
+# )
 
-ROOT_URLCONF = 'Operations_Dashboard_Django.urls'
+ROOT_URLCONF = "Operations_Dashboard_Django.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
                 # Add your custom context processors here
-                'dashboard_tools.context_processors.app_context_processor',
-                
+                "dashboard_tools.context_processors.app_context_processor",
                 # Add if needed by Dinuka's badges
-            #    'django.template.context_processors.i18n',
-            #    'django.template.context_processors.media',
-            #    'django.template.context_processors.static',
-            #    'django.template.context_processors.tz',
+                #    'django.template.context_processors.i18n',
+                #    'django.template.context_processors.media',
+                #    'django.template.context_processors.static',
+                #    'django.template.context_processors.tz',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'Operations_Dashboard_Django.wsgi.application'
+WSGI_APPLICATION = "Operations_Dashboard_Django.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'USER': CONF['DJANGO_USER'],
-        'PASSWORD': CONF['DJANGO_PASS'],
-        'HOST': os.environ.get('PGHOST', CONF.get('DB_HOSTNAME_WRITE', 'localhost')),
+    "default": {
+        "USER": CONF["DJANGO_USER"],
+        "PASSWORD": CONF["DJANGO_PASS"],
+        "HOST": os.environ.get("PGHOST", CONF.get("DB_HOSTNAME_WRITE", "localhost")),
     },
-    'default.read': {
-        'USER': CONF['DJANGO_USER'],
-        'PASSWORD': CONF['DJANGO_PASS'],
-        'HOST': os.environ.get('PGHOST', CONF.get('DB_HOSTNAME_READ', 'localhost')),
-    }
+    "default.read": {
+        "USER": CONF["DJANGO_USER"],
+        "PASSWORD": CONF["DJANGO_PASS"],
+        "HOST": os.environ.get("PGHOST", CONF.get("DB_HOSTNAME_READ", "localhost")),
+    },
 }
 for db in DATABASES:
-    DATABASES[db]['NAME'] = CONF['DB_DATABASE']
-    DATABASES[db]['ENGINE'] = 'django.db.backends.postgresql'
-    DATABASES[db]['PORT'] = os.environ.get('PGPORT', CONF.get('DB_PORT', '5432'))
-    DATABASES[db]['CONN_MAX_AGE'] = 600 # Persist DB connections
-    DATABASES[db]['OPTIONS'] = {'options': '-c search_path=dashboard'}
+    DATABASES[db]["NAME"] = CONF["DB_DATABASE"]
+    DATABASES[db]["ENGINE"] = "django.db.backends.postgresql"
+    DATABASES[db]["PORT"] = os.environ.get("PGPORT", CONF.get("DB_PORT", "5432"))
+    DATABASES[db]["CONN_MAX_AGE"] = 600  # Persist DB connections
+    # DATABASES[db]['OPTIONS'] = {'options': '-c search_path=dashboard'}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -217,62 +219,54 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_ROOT = CONF['STATIC_ROOT']
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.join( os.path.dirname(__file__),  '../static' ),
-)
+STATIC_ROOT = CONF["STATIC_ROOT"]
+STATIC_URL = "static/"
+STATICFILES_DIRS = (os.path.join(os.path.dirname(__file__), "../static"),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 # Logging setup
 import logging
 from logging.handlers import SysLogHandler
 
-if DEBUG or not os.path.exists('/dev/log'):
-    DEFAULT_LOG = 'console'
+if DEBUG or not os.path.exists("/dev/log"):
+    DEFAULT_LOG = "console"
 else:
-    DEFAULT_LOG = 'syslog'
+    DEFAULT_LOG = "syslog"
 
 LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'simple': {
-            'format': '[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s'
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "simple": {
+            "format": "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s"
         }
     },
-    'handlers': {
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': CONF['APP_LOG'],
-            'formatter': 'simple'
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": CONF["APP_LOG"],
+            "formatter": "simple",
         }
     },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'WARNING'
+    "loggers": {
+        "django": {"handlers": ["file"], "level": "WARNING"},
+        "django.server": {
+            "handlers": ["file"],
+            #            'propagate': True,
+            "level": "DEBUG",
         },
-        'django.server': {
-            'handlers': ['file'],
-#            'propagate': True,
-            'level': 'DEBUG'
-        },
-        'dashboard': {
-            'handlers': ['file'],
-            'level': 'DEBUG'
-        }
-    }
+        "dashboard": {"handlers": ["file"], "level": "DEBUG"},
+    },
 }
 
-APP_NAME = 'Operations Dashboard'
-APP_VERSION = CONF['APP_VERSION']
-#URLs for dashboard webapp
-OPERATIONS_API_BASE_URL = CONF['OPERATIONS_API_BASE_URL']
+APP_NAME = "Operations Dashboard"
+APP_VERSION = CONF["APP_VERSION"]
+# URLs for dashboard webapp
+OPERATIONS_API_BASE_URL = CONF["OPERATIONS_API_BASE_URL"]
 # For legacy Django embedded WebApp
 # OPERATIONS_INTEGRATION_BADGES_WEBAPP_VERSION = '1.0.48'
