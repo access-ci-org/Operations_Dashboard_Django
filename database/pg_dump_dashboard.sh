a9abd7cb608f1faedf9d0b485fc1bb21dd19b4aa
JP Navarro <jpnavarro@mac.com>
2026-04-07 10:02:55 -0500

    See CHANGELOG v0.22.1

--- database/pg_dump_dashboard.sh
+++ database/pg_dump_dashboard.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 DATE=`date +'%s'`
-FILE=dump/dashboard.dump.${DATE}
+FILE=dump/dashboard1.dump.${DATE}
 echo "pg_dump to: ${FILE}"
 
 pg_dump -a -p 5434 -U dashboard_django \
-    -n dashboard1 \
+    -n dashboard_django \
     dashboard1 >${FILE}
 
 echo "Manually execute:"
 echo "DROP OWNED BY dashboard_django;"
-echo "CREATE SCHEMA dashboard AUTHORIZATION dashboard_django;"
